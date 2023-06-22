bcb588999a34277be1e0a65f22df35aad097c235
JimmyRojasE <ji.rojas@duocuc.cl>
2023-06-22 15:27:05 -0400

    Commit Funciones de product

--- src/main/java/cl/musicpro/bd_service/controller/ProductController.java
+++ src/main/java/cl/musicpro/bd_service/controller/ProductController.java
@@ -3,13 +3,7 @@ package cl.musicpro.bd_service.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import cl.musicpro.bd_service.model.dto.CommonResponseDTO;
 import cl.musicpro.bd_service.model.dto.ProductDTO;
@@ -28,11 +22,28 @@ public class ProductController {
         log.info(" cl.musicpro.bd_service.controller Request:[getAll]");
         return productService.getProducts();
     }
+
+    @GetMapping(path = "/product/{id}", produces =MediaType.APPLICATION_JSON_VALUE )
+    public CommonResponseDTO getProduct(@PathVariable int id){
+        log.info(" cl.musicpro.bd_service.controller Request:[getProduct/{id}]");
+        return productService.getProduct(id);
+    }
     
     @PostMapping(path = "/product", produces = MediaType.APPLICATION_JSON_VALUE , consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<CommonResponseDTO>addProduct(@RequestBody ProductDTO productoDTO){
         log.info("[addProduct]Request: "+productoDTO.toString());
         return productService.addProduct(productoDTO);
     }
-    
+
+    @PostMapping(path = "/product/edit", produces = MediaType.APPLICATION_JSON_VALUE , consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CommonResponseDTO>EditProduct(@RequestBody ProductDTO productoDTO){
+        log.info("[editProduct]Request: "+productoDTO.toString());
+        return productService.editProduct(productoDTO);
+    }
+
+    @DeleteMapping(path = "/product/delete/{id}", produces = MediaType.APPLICATION_JSON_VALUE )
+    public CommonResponseDTO DeleteProduct(@PathVariable int id){
+        log.info("cl.musicpro.bd_service.controller Request:[DeleteProduct/{id}]");
+        return productService.deleteProduct(id);
+    }
 }
